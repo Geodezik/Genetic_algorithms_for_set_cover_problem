ae76d497dd54ca5bb4a1280513ac88e74d39dd08
Geodezik <ovs02@mail.ru>
2023-03-02 12:38:16 +0300

    Started work on Greedy implementation

---
+++ src/GreedyAlgorithm/main.cpp
@@ -0,0 +1,48 @@
+#include "Greedy.hpp"
+#include "BooleanMatrix.hpp"
+#include <iostream>
+
+using namespace Greedy;
+
+int main()
+{
+    int SEED = 317;
+    int m = 250;
+    int n = 250;
+
+    double p = 0.15;
+    std::random_device rd{};
+    std::mt19937 rng{rd()};
+    rng.seed(SEED;);
+    std::bernoulli_distribution d(p);
+
+    /*
+    bool Matrix[m][n] = {
+        {0, 1, 1, 1, 1, 0, 1, 0, 1, 0},
+        {1, 0, 1, 0, 0, 0, 1, 0, 1, 0},
+        {0, 0, 1, 0, 0, 1, 1, 0, 1, 0},
+        {0, 1, 1, 1, 1, 0, 0, 0, 1, 0},
+        {0, 0, 1, 0, 1, 0, 1, 0, 1, 0},
+        {0, 0, 1, 1, 1, 0, 0, 0, 1, 0},
+        {0, 0, 0, 0, 0, 0, 1, 0, 1, 0},
+        {0, 0, 0, 0, 0, 0, 1, 0, 1, 0},
+        {0, 0, 0, 0, 0, 0, 1, 0, 1, 0},
+        {0, 0, 0, 0, 0, 0, 0, 1, 0, 0}
+    };
+    */
+
+    BooleanMatrix::BooleanMatrix M(m, n);
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            M[i][j] = static_cast<bool> (d(rng));
+            //M[i][j] = Matrix[i][j];
+        }BooleanMatrix.o
+    }
+
+    //int population_size, int extended_population_size, int chromosome_len, double mutation_proba, int max_iter = 100, str task
+    GreedyAlgorithm A = GreedyAlgorithm();
+    A.fit(M);
+    //A.print_solution(M);
+
+    return 0;
+}
